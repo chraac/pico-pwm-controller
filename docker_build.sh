d0b4dde242bbb08f1b05711f577469404830fb6b
hongruichen <chraac@gmail.com>
2022-01-12 00:16:24 +0800

    add docker build script

---
+++ docker_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z $PICO_BASE_PATH ]; then 
+    echo 'Variable PICO_BASE_PATH not set'
+    exit 1
+else
+    echo "PICO_BASE_PATH: $PICO_BASE_PATH"
+fi
+
+set -e
+PROJECT_PATH=$(pwd)
+echo "PROJECT_PATH: $PROJECT_PATH"
+
+docker run --rm -it \
+    -v $PICO_BASE_PATH:/pico \
+    -v $PROJECT_PATH:/project \
+    -w=/project \
+    chraac/pico-builder:latest \
+    bash -c 'mkdir -p build && cd build && cmake .. && make'
