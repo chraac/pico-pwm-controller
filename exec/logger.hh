c1388573fe1e18aa2af550000deb59879e11f132
chr <chraac@gmail.com>
2022-03-16 17:59:27 +0800

    wip

--- exec/logger.hh
+++ exec/logger.hh
@@ -4,4 +4,9 @@
 #include "base_types.hh"
 
 #define log_info(format,args...) printf(format, ## args)
+#ifdef DEBUG
 #define log_debug(format,args...) printf(format, ## args)
+#else
+#define log_debug(format,args...) (void)0
+#endif
+
