a9bb3e91aaa21ca566d6841cc4cc4aa4c06d572b
chr <chraac@gmail.com>
2022-04-23 10:47:21 +0800

    wip

---
+++ exec/frequency_counter.hh
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <hardware/gpio.h>
+#include <hardware/timer.h>
+
+#include <atomic>
+
+#include "base_types.hh"
+#include "critical_section_helper.hh"
+
+namespace utility {
+
+constexpr uint32_t kGpioPinCount = 30;
+
+class GpioFreqencyCounter : public GpioBase {
+public:
+    GpioFreqencyCounter(const uint gpio_pin) noexcept : GpioBase(gpio_pin) {
+        gpio_set_irq_enabled_with_callback(
+            gpio_pin, GPIO_IRQ_EDGE_RISE, true,
+            &GpioFreqencyCounter::GpioEventHandler);
+    }
+
+    uint32_t GetFrequencyHz() noexcept {
+        uint32_t count;
+        const auto now_us = time_us_64();
+        event_count_critical_section_.Lock();
+        count = event_count_[gpio_pin_];
+        event_count_[gpio_pin_] = 0;
+        event_count_critical_section_.Unlock();
+        const auto interval_us = (now_us - last_time_us_);
+        last_time_us_ = now_us;
+        return uint64_t(count) * 1000000 / interval_us;
+    }
+
+    void Reset() noexcept {
+        event_count_critical_section_.Lock();
+        event_count_[gpio_pin_] = 0;
+        event_count_critical_section_.Unlock();
+        last_time_us_ = time_us_64();
+    }
+
+private:
+    static void GpioEventHandler(uint gpio, uint32_t events) {
+        if (events & GPIO_IRQ_EDGE_RISE) {
+            event_count_critical_section_.Lock();
+            ++event_count_[gpio];
+            event_count_critical_section_.Unlock();
+        }
+    }
+
+    static uint32_t event_count_[kGpioPinCount];
+    static CriticalSection event_count_critical_section_;
+
+    uint64_t last_time_us_ = time_us_64();
+
+    DISALLOW_COPY(GpioFreqencyCounter);
+    DISALLOW_MOVE(GpioFreqencyCounter);
+};
+
+uint32_t GpioFreqencyCounter::event_count_[] = {};
+CriticalSection GpioFreqencyCounter::event_count_critical_section_;
+
+}  // namespace utility
