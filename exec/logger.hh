022954f6681486399abf820dd7d9913a581d9066
chr <chraac@gmail.com>
2022-05-03 23:51:47 +0800

    port logger for esp32

--- exec/logger.hh
+++ exec/logger.hh
@@ -1,12 +1,29 @@
 #pragma once
 
-#include <pico/stdio.h>
 
 #include "base_types.hh"
+
+#ifdef PLATFORM_PICO
+
+#include <pico/stdio.h>
 
 #define log_info(format, args...) printf(format, ##args)
 #ifdef DEBUG
 #define log_debug(format, args...) printf(format, ##args)
 #else
 #define log_debug(format, args...) (void)0
+#endif
+
+#elif defined(PLATFORM_ESP32)
+
+#include <esp_log.h>
+
+#define LOG_TAG "pwm"
+#define log_info(format, args...) ESP_LOGI(LOG_TAG, format, ##args)
+#ifdef DEBUG
+#define log_debug(format, args...) ESP_LOGD(LOG_TAG, format, ##args)
+#else
+#define log_debug(format, args...) (void)0
+#endif
+
 #endif
