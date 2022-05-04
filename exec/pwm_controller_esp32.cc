09189bb74965d75c409c1aab0ea19ec268074d96
chr <chraac@gmail.com>
2022-05-04 23:34:51 +0800

    reformat

--- exec/pwm_controller_esp32.cc
+++ exec/pwm_controller_esp32.cc
@@ -1,9 +1,7 @@
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+
 #include "logger.hh"
 
-extern "C" void app_main(void)
-{
-    log_debug("app_main.init\n");
-}
+extern "C" void app_main(void) { log_debug("app_main.init\n"); }
