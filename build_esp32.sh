491c28c03153ce17fe82af41519f3a5422649fe9
chr <chraac@gmail.com>
2022-05-08 13:35:52 +0800

    add debug flag

--- build_esp32.sh
+++ build_esp32.sh
@@ -4,4 +4,4 @@ set -e
 PROJECT_PATH=$(pwd)
 echo "PROJECT_PATH: $PROJECT_PATH"
 
-docker run --rm -v $PROJECT_PATH:/project -w /project espressif/idf:v4.4.1 idf.py build
+docker run --rm -v $PROJECT_PATH:/project -w /project espressif/idf:v4.4.1 idf.py -DCMAKE_BUILD_TYPE=Debug build
