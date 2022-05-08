47b80128eed8aee06198635ee22ecba4dc265f45
chr <chraac@gmail.com>
2022-05-08 13:42:01 +0800

    wip

--- build_esp32.sh
+++ build_esp32.sh
@@ -4,4 +4,4 @@ set -e
 PROJECT_PATH=$(pwd)
 echo "PROJECT_PATH: $PROJECT_PATH"
 
-docker run --rm -v $PROJECT_PATH:/project -w /project espressif/idf:v4.4.1 idf.py -DCMAKE_BUILD_TYPE=Debug build
+docker run --rm -e IDF_TARGET=esp32c3 -v $PROJECT_PATH:/project -w /project espressif/idf:v4.4.1 idf.py -DCMAKE_BUILD_TYPE=Debug build
