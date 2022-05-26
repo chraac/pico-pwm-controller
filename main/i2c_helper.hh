f977de9675144fb987b8bb3f7f6bcf0862f5cea0
hongruichen <chraac@gmail.com>
2022-05-26 11:30:31 +0800

    fix esp32 build

--- main/i2c_helper.hh
+++ main/i2c_helper.hh
@@ -7,14 +7,25 @@
 namespace utility {
 typedef i2c_inst_t *i2c_handler_t;
 }
+#elif defined(PLATFORM_ESP32)
+#include <driver/i2c.h>
+namespace utility {
+typedef i2c_cmd_handle_t i2c_handler_t;
+}
 #endif
 
 namespace utility {
 
 class I2cHelper {
 public:
+#ifdef PLATFORM_PICO
     I2cHelper(uint32_t gpio_scl, uint32_t gpio_sda,
               uint32_t baudrate_hz) noexcept;
+#elif defined(PLATFORM_ESP32)
+    I2cHelper(uint32_t gpio_scl, uint32_t gpio_sda, uint32_t baudrate_hz,
+              uint32_t i2c_port = 0) noexcept;
+#endif
+
     ~I2cHelper() noexcept;
 
     int Read(uint8_t addr, uint8_t *dst, size_t len) noexcept;
@@ -25,6 +36,9 @@ private:
     uint32_t gpio_sda_;
     uint32_t baudrate_hz_;
     i2c_handler_t handler_ = nullptr;
+#ifdef PLATFORM_ESP32
+    uint32_t i2c_master_port_ = 0;
+#endif
 
     DISALLOW_COPY(I2cHelper);
     DISALLOW_MOVE(I2cHelper);
