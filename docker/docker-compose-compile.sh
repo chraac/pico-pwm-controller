95c022dfa7a52c48ccc041c499b091f01b30cf49
nullname <chraac@gmail.com>
2024-12-22 17:32:20 +0800

    feat: add rp2350 config options (#8)
    * add build param for 2350
    
    * bump pico-sdk version to 2.1.0

--- docker/docker-compose-compile.sh
+++ docker/docker-compose-compile.sh
@@ -1,6 +1,23 @@
 _script_dir="$(dirname "$0")"
 _repo_dir=$(realpath "$_script_dir/..")
 _output_dir="$_repo_dir/build"
+_board_name='seeed_xiao_rp2040'
+
+#parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+    -rp2350)
+        _board_name="seeed_xiao_rp2350"
+        shift
+        shift
+        ;;
+    *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
 
 pushd "$_script_dir"
 
@@ -8,7 +25,7 @@ mkdir -p $_output_dir
 
 set -e
 
-OUTPUT_DIR=$_output_dir docker compose -f docker-compose-compile.yml up --build
+OUTPUT_DIR=$_output_dir PICO_BOARD=$_board_name docker compose -f docker-compose-compile.yml up --build
 
 set +e
 
